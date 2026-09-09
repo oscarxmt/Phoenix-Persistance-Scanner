d140efee8f854369015530c5437057878c4a9240
Oscar <148647012+oscarxmt@users.noreply.github.com>
2026-09-09 12:39:01 +0200

    Attempt 1 for adding json to the powershell scripts for quicker looks

--- Windows/startup_folders.ps1
+++ Windows/startup_folders.ps1
@@ -9,3 +9,15 @@ $finalfilename = $user + "_ScheduledTask"
 Write-Host "`n[*] Checking Startup Folders..."
 Get-ChildItem "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup"
 Get-ChildItem "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup"
+
+
+[Json]$MyJsonVariable = "
+{
+    ""Startup_Folders"": {
+        ""User_Startup_Folder"": ""$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup"",
+        ""All_Users_Startup_Folder"": ""C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup""
+    }
+
+
+}
+"
